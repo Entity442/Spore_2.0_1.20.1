aebaac62eab9985a4a3d403a661a5f02dff0ec42
Entity442 <mihailungoci87@gmail.com>
2023-10-02 05:09:07 -0700

    new stuff

--- src/main/java/com/Harbinger/Spore/Sentities/BaseEntities/Organoid.java
+++ src/main/java/com/Harbinger/Spore/Sentities/BaseEntities/Organoid.java
@@ -59,7 +59,7 @@ public class Organoid extends UtilityEntity implements Enemy {
         this.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>
                 (this, Player.class,  true));
         this.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(this, LivingEntity.class, 5, false, true, (en) -> {
-            return SConfig.SERVER.whitelist.get().contains(en.getEncodeId()) || en.hasEffect(Seffects.MARKER.get());
+            return (SConfig.SERVER.whitelist.get().contains(en.getEncodeId()) || en.hasEffect(Seffects.MARKER.get())) && !(en instanceof Infected || en instanceof UtilityEntity);
         }));
         this.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(this, LivingEntity.class, 5, false, true, (en) -> {
             return !(this.otherWorld(en) || this.SkulkLove(en) || this.likedFellows(en)) && SConfig.SERVER.at_mob.get();
