1a18073e37af01521892769eeb5cee3093d5ec47
Entity442 <mihailungoci87@gmail.com>
2023-12-18 10:52:06 -0800

    new blocks fml

--- src/main/java/com/Harbinger/Spore/Sentities/BaseEntities/EvolvedInfected.java
+++ src/main/java/com/Harbinger/Spore/Sentities/BaseEntities/EvolvedInfected.java
@@ -1,13 +1,10 @@
 package com.Harbinger.Spore.Sentities.BaseEntities;
 
+import com.Harbinger.Spore.Sentities.EvolvedInfected.Scamper;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.monster.Monster;
-import net.minecraft.world.entity.vehicle.Boat;
 import net.minecraft.world.level.Level;
-
-import java.util.Objects;
 
 public class EvolvedInfected extends Infected {
     public EvolvedInfected(EntityType<? extends Monster> type, Level level) {
@@ -24,6 +21,6 @@ public class EvolvedInfected extends Infected {
 
     @Override
     public boolean removeWhenFarAway(double p_21542_) {
-        return this.getLinked();
+        return this.getLinked() && !(this instanceof Scamper);
     }
 }
