2182a63add84a3b6f517748a00c460de449a3407
Entity_Key_Whisper <mihailungoci87@gmail.com>
2024-04-08 12:38:43 -0700

    spawn item

--- src/main/java/com/Harbinger/Spore/Core/Sitems.java
+++ src/main/java/com/Harbinger/Spore/Core/Sitems.java
@@ -181,6 +181,9 @@ public class Sitems {
     public  static final RegistryObject<Item> PROTO_SPAWNEGG = ITEMS.register("proto_spawnegg",
             () -> new SporeSpawnEgg(Sentities.PROTO,244208, new Item.Properties()));
 
+    public  static final RegistryObject<Item> WENDIGO_SPAWNEGG = ITEMS.register("wendigo_spawnegg",
+            () -> new SporeSpawnEgg(Sentities.WENDIGO,-354345818, new Item.Properties()));
+
     public  static final RegistryObject<Item> SIEGER_SPAWNEGG = ITEMS.register("sieger_spawnegg",
             () -> new SporeSpawnEgg(Sentities.SIEGER,244208, new Item.Properties()));
 
