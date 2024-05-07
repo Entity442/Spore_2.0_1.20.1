9a647c32fb358ccec7503916ee969682c596826a
Entity_Key_Whisper <mihailungoci87@gmail.com>
2024-05-07 11:51:12 -0700

    techno

--- src/main/java/com/Harbinger/Spore/Client/Renderers/InfectedPlayerRenderer.java
+++ src/main/java/com/Harbinger/Spore/Client/Renderers/InfectedPlayerRenderer.java
@@ -10,14 +10,19 @@ import net.minecraft.client.model.geom.ModelLayers;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.layers.HumanoidArmorLayer;
 import net.minecraft.client.renderer.entity.layers.ItemInHandLayer;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+
+import java.util.Objects;
 
 @OnlyIn(Dist.CLIENT)
 public class InfectedPlayerRenderer<Type extends InfectedPlayer> extends BaseInfectedRenderer<Type , InfectedPlayerModel<Type>> {
     private static final ResourceLocation TEXTURE = new ResourceLocation(Spore.MODID,
             "textures/entity/inf_player.png");
+    private static final ResourceLocation TECHNO = new ResourceLocation(Spore.MODID,
+            "textures/entity/techno_skin.png");
     private static final ResourceLocation EYES_TEXTURE = new ResourceLocation(Spore.MODID,
             "textures/entity/eyes/inf_player.png");
 
@@ -35,6 +40,9 @@ public class InfectedPlayerRenderer<Type extends InfectedPlayer> extends BaseInf
 
     @Override
     public ResourceLocation getTextureLocation(Type entity) {
+        if (Objects.equals(entity.getCustomName(), Component.literal("Technoblade"))) {
+            return TECHNO;
+        }
         return TEXTURE;
     }
 
