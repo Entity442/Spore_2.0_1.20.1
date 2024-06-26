2f7a151ad24e0d63851d8934f8c0590f2f129b9f
Entity_Key_Whisper <mihailungoci87@gmail.com>
2024-06-26 00:18:08 -0700

    changes

--- src/main/java/com/Harbinger/Spore/Sentities/BaseEntities/Infected.java
+++ src/main/java/com/Harbinger/Spore/Sentities/BaseEntities/Infected.java
@@ -304,6 +304,9 @@ public class Infected extends Monster{
 
     @Override
     public boolean removeWhenFarAway(double p_21542_) {
+        if (this.getEvoPoints() < SConfig.SERVER.min_kills.get()){
+            return true;
+        }
         return !this.entityData.get(PERSISTENT);
     }
 
