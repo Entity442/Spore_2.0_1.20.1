16daf20a25b92ed5919b727c30bc5e288c0ee5bc
Harby <mihailungoci87@gmail.com>
2024-07-11 09:16:42 -0700

    zoaholic tinkering

--- src/main/java/com/Harbinger/Spore/Sblocks/ZoaholicBlock.java
+++ src/main/java/com/Harbinger/Spore/Sblocks/ZoaholicBlock.java
@@ -12,7 +12,6 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
