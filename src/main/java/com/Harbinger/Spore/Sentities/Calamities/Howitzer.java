4e9437a94927ed1cf7e56793805aa67af56f2870
Harby <mihailungoci87@gmail.com>
2024-07-31 07:35:40 -0700

    howitzer

--- src/main/java/com/Harbinger/Spore/Sentities/Calamities/Howitzer.java
+++ src/main/java/com/Harbinger/Spore/Sentities/Calamities/Howitzer.java
@@ -1,10 +1,13 @@
 package com.Harbinger.Spore.Sentities.Calamities;
 
 import com.Harbinger.Spore.Core.SConfig;
+import com.Harbinger.Spore.Core.Ssounds;
 import com.Harbinger.Spore.Sentities.AI.AOEMeleeAttackGoal;
 import com.Harbinger.Spore.Sentities.BaseEntities.Calamity;
 import com.Harbinger.Spore.Sentities.BaseEntities.CalamityMultipart;
 import com.Harbinger.Spore.Sentities.TrueCalamity;
+import net.minecraft.network.protocol.game.ClientboundAddEntityPacket;
+import net.minecraft.util.Mth;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
@@ -13,15 +16,27 @@ import net.minecraft.world.entity.PathfinderMob;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
 
 public class Howitzer extends Calamity implements TrueCalamity {
+    private final CalamityMultipart[] subEntities;
+    public final CalamityMultipart rightArm;
+    public final CalamityMultipart leftArm;
+    public final CalamityMultipart mouth;
     public Howitzer(EntityType<? extends PathfinderMob> type, Level level) {
         super(type, level);
+        this.rightArm = new CalamityMultipart(this, "rightarm", 2F, 4F);
+        this.leftArm = new CalamityMultipart(this, "leftarm", 2F, 4F);
+        this.mouth = new CalamityMultipart(this, "mouth", 4F, 3F);
+        this.subEntities = new CalamityMultipart[]{ this.rightArm, this.leftArm,this.mouth};
+        this.setMaxUpStep(1.5F);
+        this.setId(ENTITY_COUNTER.getAndAdd(this.subEntities.length + 1) + 1);
     }
-
     @Override
-    public boolean hurt(CalamityMultipart calamityMultipart, DamageSource source, float value) {
-        return false;
+    public void setId(int p_20235_) {
+        super.setId(p_20235_);
+        for (int i = 0; i < this.subEntities.length; i++)
+            this.subEntities[i].setId(p_20235_ + i + 1);
     }
 
     @Override
@@ -51,4 +66,66 @@ public class Howitzer extends Calamity implements TrueCalamity {
                 .add(Attributes.ATTACK_KNOCKBACK, 2);
 
     }
+
+    @Override
+    public void aiStep() {
+        float f14 = this.getYRot() * ((float)Math.PI/180);
+        float f2 = Mth.sin(f14);
+        float f15 = Mth.cos(f14);
+        Vec3[] avec3 = new Vec3[this.subEntities.length];
+        for(int j = 0; j < this.subEntities.length; ++j) {
+            avec3[j] = new Vec3(this.subEntities[j].getX(), this.subEntities[j].getY(), this.subEntities[j].getZ());
+        }
+        this.tickPart(this.mouth, (double)(f2*0.5f), 5.0D, (double)(-f15 *0.5f));
+        this.tickPart(this.rightArm, (double)(f2 * 5F), 0.0D, (double)(-f15*3f));
+        this.tickPart(this.leftArm, (double)(f2 *-5.2F), 0.0D, (double)(-f15 * -2f));
+        for(int l = 0; l < this.subEntities.length; ++l) {
+            this.subEntities[l].xo = avec3[l].x;
+            this.subEntities[l].yo = avec3[l].y;
+            this.subEntities[l].zo = avec3[l].z;
+            this.subEntities[l].xOld = avec3[l].x;
+            this.subEntities[l].yOld = avec3[l].y;
+            this.subEntities[l].zOld = avec3[l].z;
+        }
+        super.aiStep();
+    }
+
+    public CalamityMultipart[] getSubEntities() {
+        return this.subEntities;
+    }
+
+    @Override
+    public boolean isMultipartEntity() {
+        return true;
+    }
+
+    @Override
+    public net.minecraftforge.entity.PartEntity<?>[] getParts() {
+        return this.subEntities;
+    }
+
+    public void recreateFromPacket(ClientboundAddEntityPacket p_218825_) {
+        super.recreateFromPacket(p_218825_);
+        if (true) return;
+        CalamityMultipart[] calamityMultiparts = this.getSubEntities();
+
+        for(int i = 0; i < calamityMultiparts.length; ++i) {
+            calamityMultiparts[i].setId(i + p_218825_.getId());
+        }
+
+    }
+    public boolean hurt(CalamityMultipart calamityMultipart, DamageSource source, float value) {
+        if (calamityMultipart == this.mouth){
+            this.hurt(source,value*2f);
+        }else if (calamityMultipart == this.rightArm){
+            this.hurt(source,value *1.5f);
+        }else if (calamityMultipart == this.leftArm){
+            this.hurt(source,value*1.5f);
+        } else{
+            this.hurt(source,value );
+        }
+        return true;
+    }
+
+
 }
