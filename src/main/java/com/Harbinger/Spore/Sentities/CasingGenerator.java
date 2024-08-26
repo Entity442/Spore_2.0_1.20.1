632c57030d8c22cf5c92e10ac0b595071dd55a4f
Harby <mihailungoci87@gmail.com>
2024-08-26 08:11:42 -0700

    bugs

--- src/main/java/com/Harbinger/Spore/Sentities/CasingGenerator.java
+++ src/main/java/com/Harbinger/Spore/Sentities/CasingGenerator.java
@@ -4,6 +4,7 @@ import com.Harbinger.Spore.Core.SConfig;
 import com.Harbinger.Spore.Core.Sblocks;
 import com.Harbinger.Spore.Sblocks.MembraneBlock;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
@@ -35,6 +36,9 @@ public interface CasingGenerator {
         List<BlockState> values = new ArrayList<>();
         values.add(Sblocks.BIOMASS_BLOCK.get().defaultBlockState());
         values.add(Sblocks.BIOMASS_BLOCK.get().defaultBlockState());
+        values.add(Sblocks.BIOMASS_BLOCK.get().defaultBlockState());
+        values.add(Sblocks.ROOTED_BIOMASS.get().defaultBlockState());
+        values.add(Sblocks.ROOTED_BIOMASS.get().defaultBlockState());
         values.add(Sblocks.ROOTED_BIOMASS.get().defaultBlockState());
         values.add(Sblocks.CALCIFIED_BIOMASS_BLOCK.get().defaultBlockState());
         values.add(Sblocks.SICKEN_BIOMASS_BLOCK.get().defaultBlockState());
@@ -57,6 +61,15 @@ public interface CasingGenerator {
                             if (Math.random() < 0.1 && !blockstate.isSolidRender(level,blockpos) && compare(level,blockpos)){
                                 if (!level.isClientSide){
                                     level.setBlock(blockpos,this.possibleBlocks().get(randomSource.nextInt(this.possibleBlocks().size())),3);
+                                    if (Math.random() < 0.001){
+                                        createSpots(blockpos,level,randomSource.nextInt(3,6),Math.random() < 0.5 ? Sblocks.SICKEN_BIOMASS_BLOCK.get().defaultBlockState():Sblocks.CALCIFIED_BIOMASS_BLOCK.get().defaultBlockState());
+                                    }
+                                    if (Math.random() < 0.001){
+                                        createPussSpots(blockpos,level,randomSource.nextInt(2,5));
+                                    }
+                                    if (Math.random() < 0.01 && level.getBlockState(blockpos.above()).isAir()){
+                                        createFungalStalks(blockpos,level,randomSource.nextInt(4,12),randomSource);
+                                    }
                                 }
                             }
                             if (Math.random() < 0.05 && blockstate.isSolidRender(level,blockpos)){
@@ -76,8 +89,49 @@ public interface CasingGenerator {
                 }
             }
         }
+    }
 
-
-
+    default void createSpots(BlockPos pos,Level level, int range,BlockState state){
+        for(int i = 0; i <= 2*range; ++i) {
+            for(int j = 0; j <= 2*range; ++j) {
+                for(int k = 0; k <= 2*range; ++k) {
+                    double distance = Mth.sqrt((float) ((i-range)*(i-range) + (j-range)*(j-range) + (k-range)*(k-range)));
+                    if (Math.abs(i) != 2 || Math.abs(j) != 2 || Math.abs(k) != 2) {
+                        if (distance<range+(0.5)){
+                            BlockPos blockpos = pos.offset( i-(int)range,j-(int)range,k-(int)range);
+                            if (possibleBlocks().contains(level.getBlockState(blockpos))){
+                                level.setBlock(blockpos,state,3);
+                            }
+                        }}}}}
+    }
+    default void createPussSpots(BlockPos pos,Level level, int range){
+        for(int i = 0; i <= 2*range; ++i) {
+            for(int j = 0; j <= 2*range; ++j) {
+                for(int k = 0; k <= 2*range; ++k) {
+                    double distance = Mth.sqrt((float) ((i-range)*(i-range) + (j-range)*(j-range) + (k-range)*(k-range)));
+                    if (Math.abs(i) != 2 || Math.abs(j) != 2 || Math.abs(k) != 2) {
+                        if (distance<range+(0.5) && distance>range-(0.5)){
+                            BlockPos blockpos = pos.offset( i-(int)range,j-(int)range,k-(int)range);
+                            if (level.getBlockState(blockpos).isAir()){
+                                level.setBlock(blockpos,Math.random() < 0.5 ? Sblocks.SICKEN_BIOMASS_BLOCK.get().defaultBlockState() : Sblocks.BIOMASS_BLOCK.get().defaultBlockState(), 3);
+                            }
+                        }
+                        if (distance<range-(0.5)){
+                            BlockPos blockpos = pos.offset( i-(int)range,j-(int)range,k-(int)range);
+                            if (level.getBlockState(blockpos).isAir()){
+                                level.setBlock(blockpos,Math.random() < 0.5 ? Sblocks.SICKEN_BIOMASS_BLOCK.get().defaultBlockState() : Sblocks.BILE.get().defaultBlockState(), 3);
+                            }
+                        }
+                    }}}}
+    }
+    default void createFungalStalks(BlockPos pos,Level level,int range,RandomSource source){
+        List<Integer> offsets = new ArrayList<>(){{add(-1);add(0);add(1);}};
+        List<Integer> offsets2 = new ArrayList<>(){{add(-1);add(0);add(1);}};
+        List<BlockState> states = new ArrayList<>(){{add(Sblocks.MYCELIUM_BLOCK.get().defaultBlockState());add(Sblocks.FUNGAL_SHELL.get().defaultBlockState());}};
+        BlockPos blockPos = pos;
+        for (int i = 0; i <= range;i++){
+            blockPos = blockPos.offset(offsets.get(source.nextInt(offsets.size())),i,offsets2.get(source.nextInt(offsets.size())));
+            level.setBlock(blockPos,states.get(source.nextInt(states.size())),3);
+        }
     }
 }
