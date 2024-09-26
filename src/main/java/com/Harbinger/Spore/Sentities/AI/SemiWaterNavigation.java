dfb284e6eca21665db3c7041c73115596e27dd97
Harby <mihailungoci87@gmail.com>
2024-09-26 10:49:14 -0700

    reworks

--- src/main/java/com/Harbinger/Spore/Sentities/AI/SemiWaterNavigation.java
+++ src/main/java/com/Harbinger/Spore/Sentities/AI/SemiWaterNavigation.java
@@ -1,19 +1,25 @@
 package com.Harbinger.Spore.Sentities.AI;
 
 import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.ai.navigation.GroundPathNavigation;
 import net.minecraft.world.entity.ai.navigation.WaterBoundPathNavigation;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.pathfinder.PathFinder;
 import net.minecraft.world.level.pathfinder.SwimNodeEvaluator;
+import net.minecraft.world.level.pathfinder.WalkNodeEvaluator;
 
-public class SemiWaterNavigation extends WaterBoundPathNavigation {
+public class SemiWaterNavigation extends GroundPathNavigation {
     public SemiWaterNavigation(Mob p_26594_, Level p_26595_) {
         super(p_26594_, p_26595_);
     }
 
     @Override
     protected PathFinder createPathFinder(int p_26598_) {
-        this.nodeEvaluator = new SwimNodeEvaluator(true);
+        if (isInLiquid()){
+            this.nodeEvaluator = new SwimNodeEvaluator(true);
+        }else{
+            this.nodeEvaluator = new WalkNodeEvaluator();
+        }
         return new PathFinder(this.nodeEvaluator, p_26598_);
     }
 }
