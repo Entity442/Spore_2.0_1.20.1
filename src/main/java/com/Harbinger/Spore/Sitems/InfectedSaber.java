08247940cf271a20a0af73f0f0c85fc6dabe713f
mihai <mihailungoci87@gmail.com>
2025-01-08 11:47:59 -0800

    further work

--- src/main/java/com/Harbinger/Spore/Sitems/InfectedSaber.java
+++ src/main/java/com/Harbinger/Spore/Sitems/InfectedSaber.java
@@ -2,10 +2,9 @@ package com.Harbinger.Spore.Sitems;
 
 import com.Harbinger.Spore.Core.SConfig;
 import com.Harbinger.Spore.Sitems.BaseWeapons.SporeSwordBase;
-import net.minecraft.stats.Stats;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
@@ -16,24 +15,17 @@ public class InfectedSaber extends SporeSwordBase {
     }
 
     @Override
-    public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
-        ItemStack stack = pPlayer.getItemInHand(pUsedHand);
-        if (!tooHurt(stack)){
-            return InteractionResultHolder.fail(pPlayer.getItemInHand(pUsedHand));
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
+        if (player instanceof ServerPlayer serverPlayer) {
+            leap(serverPlayer);
+            return InteractionResultHolder.success(player.getItemInHand(hand));
         }
-        pPlayer.awardStat(Stats.ITEM_USED.get(this));
-        pPlayer.startUsingItem(pUsedHand);
-        return InteractionResultHolder.consume(pPlayer.getItemInHand(pUsedHand));
+        return super.use(level, player, hand);
     }
 
-    @Override
-    public void onStopUsing(ItemStack stack, LivingEntity entity, int count) {
-        if (!entity.level().isClientSide() && count < 1) {
-            entity.hurtMarked = true;
-            entity.knockback(4.5F, -entity.getLookAngle().x, -entity.getLookAngle().z);
-            if (entity instanceof Player player){
-                player.getCooldowns().addCooldown(this,20);
-            }
-        }
+    public void leap(Player player) {
+        player.hurtMarked = true;
+        player.knockback(2.5F, -player.getLookAngle().x, -player.getLookAngle().z);
+        player.getCooldowns().addCooldown(this,40);
     }
 }
--- src/main/java/com/Harbinger/Spore/Sitems/InfectedScythe.java
+++ src/main/java/com/Harbinger/Spore/Sitems/InfectedScythe.java
@@ -1,17 +1,67 @@
 package com.Harbinger.Spore.Sitems;
 
 import com.Harbinger.Spore.Core.SConfig;
-import com.Harbinger.Spore.Sitems.BaseWeapons.SporeToolsBaseItem;
+import com.Harbinger.Spore.Sitems.BaseWeapons.SporeDiggerTools;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
+import com.mojang.datafixers.util.Pair;
+import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.HoeItem;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraft.world.phys.AABB;
 import net.minecraftforge.common.ToolActions;
 import org.jetbrains.annotations.NotNull;
 
-public class InfectedScythe extends SporeToolsBaseItem {
+import java.util.Map;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+public class InfectedScythe extends SporeDiggerTools {
+    protected static final Map<Block, Pair<Predicate<UseOnContext>, Consumer<UseOnContext>>> TILLABLES;
     public InfectedScythe() {
-        super(SConfig.SERVER.scythe_damage.get(), 3f, 3F, SConfig.SERVER.scythe_durability.get(), 3);
+        super(SConfig.SERVER.scythe_damage.get(), 3f, 3F, SConfig.SERVER.scythe_durability.get(), 3, BlockTags.MINEABLE_WITH_HOE);
+    }
+
+    public InteractionResult useOn(UseOnContext context) {
+        Level level = context.getLevel();
+        BlockPos blockpos = context.getClickedPos();
+        Player player = context.getPlayer();
+        BlockState toolModifiedState = level.getBlockState(blockpos).getToolModifiedState(context, ToolActions.HOE_TILL, false);
+        Pair<Predicate<UseOnContext>, Consumer<UseOnContext>> pair = toolModifiedState == null ? null : Pair.of((ctx) -> {
+            return true;
+        }, changeIntoState(toolModifiedState));
+        if (pair == null) {
+            return InteractionResult.PASS;
+        } else {
+            Predicate<UseOnContext> predicate = (Predicate)pair.getFirst();
+            Consumer<UseOnContext> consumer = (Consumer)pair.getSecond();
+            if (predicate.test(context)) {
+                level.playSound(player, blockpos, SoundEvents.HOE_TILL, SoundSource.BLOCKS, 1.0F, 1.0F);
+                if (!level.isClientSide) {
+                    consumer.accept(context);
+                    if (player != null){
+                        hurtTool(context.getItemInHand(),player,1);
+                    }
+                }
+                return InteractionResult.sidedSuccess(level.isClientSide);
+            } else {
+                return InteractionResult.PASS;
+            }
+        }
     }
 
     @Override
@@ -22,6 +72,27 @@ public class InfectedScythe extends SporeToolsBaseItem {
     @Override
     public @NotNull AABB getSweepHitBox(@NotNull ItemStack stack, @NotNull Player player, @NotNull Entity target) {
         return target.getBoundingBox().inflate(4);
+    }
+
+    public static Consumer<UseOnContext> changeIntoState(BlockState p_150859_) {
+        return (p_238241_) -> {
+            p_238241_.getLevel().setBlock(p_238241_.getClickedPos(), p_150859_, 11);
+            p_238241_.getLevel().gameEvent(GameEvent.BLOCK_CHANGE, p_238241_.getClickedPos(), GameEvent.Context.of(p_238241_.getPlayer(), p_150859_));
+        };
+    }
+
+    public static Consumer<UseOnContext> changeIntoStateAndDropItem(BlockState p_150850_, ItemLike p_150851_) {
+        return (p_238246_) -> {
+            p_238246_.getLevel().setBlock(p_238246_.getClickedPos(), p_150850_, 11);
+            p_238246_.getLevel().gameEvent(GameEvent.BLOCK_CHANGE, p_238246_.getClickedPos(), GameEvent.Context.of(p_238246_.getPlayer(), p_150850_));
+            Block.popResourceFromFace(p_238246_.getLevel(), p_238246_.getClickedPos(), p_238246_.getClickedFace(), new ItemStack(p_150851_));
+        };
+    }
+
+    static {
+        TILLABLES = Maps.newHashMap(ImmutableMap.of(Blocks.GRASS_BLOCK, Pair.of(HoeItem::onlyIfAirAbove, changeIntoState(Blocks.FARMLAND.defaultBlockState())), Blocks.DIRT_PATH, Pair.of(HoeItem::onlyIfAirAbove, changeIntoState(Blocks.FARMLAND.defaultBlockState())), Blocks.DIRT, Pair.of(HoeItem::onlyIfAirAbove, changeIntoState(Blocks.FARMLAND.defaultBlockState())), Blocks.COARSE_DIRT, Pair.of(HoeItem::onlyIfAirAbove, changeIntoState(Blocks.DIRT.defaultBlockState())), Blocks.ROOTED_DIRT, Pair.of((p_238242_) -> {
+            return true;
+        }, changeIntoStateAndDropItem(Blocks.DIRT.defaultBlockState(), Items.HANGING_ROOTS))));
     }
 }
 
