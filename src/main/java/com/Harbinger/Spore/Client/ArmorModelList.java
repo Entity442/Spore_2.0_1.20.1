954feaa1b09e5321b2f6dfa13fd3a58d10d00a56
mihai <mihailungoci87@gmail.com>
2025-01-20 09:03:38 -0800

    polishing

--- src/main/java/com/Harbinger/Spore/Client/ArmorModelList.java
+++ src/main/java/com/Harbinger/Spore/Client/ArmorModelList.java
@@ -23,6 +23,7 @@ public class ArmorModelList {
         map.add(new Triple<>(new CoreModel<>(),Sitems.LIVING_CHEST.get(),new ResourceLocation(Spore.MODID,"textures/armor/infected_wing.png")));
         map.add(new Triple<>(new CoreModel<>(),Sitems.INF_CHEST.get(),new ResourceLocation(Spore.MODID,"textures/armor/infected_wing.png")));
         map.add(new Triple<>(new ElytrumModel<>(),Sitems.INF_UP_CHESTPLATE.get(),new ResourceLocation(Spore.MODID,"textures/armor/elytron.png")));
+        map.add(new Triple<>(new CoreModel<>(),Sitems.INF_UP_CHESTPLATE.get(),new ResourceLocation(Spore.MODID,"textures/armor/infected_wing.png")));
         return map;
     }
 }
