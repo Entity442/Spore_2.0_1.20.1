ff2a7222f5f0c01034fdab5397e266872586e2ca
mihai <mihailungoci87@gmail.com>
2025-04-16 05:46:14 -0700

    experiment love

--- src/main/java/com/Harbinger/Spore/Sentities/BaseEntities/Experiment.java
+++ src/main/java/com/Harbinger/Spore/Sentities/BaseEntities/Experiment.java
@@ -10,6 +10,7 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EntityType;
@@ -44,6 +45,7 @@ public class Experiment extends Infected{
     }
     public void setDormant(boolean value){
         entityData.set(DORMANT,value);
+        if (!value){this.playSound(SoundEvents.CROP_BREAK);}
     }
     @Override
     public void readAdditionalSaveData(CompoundTag tag) {
