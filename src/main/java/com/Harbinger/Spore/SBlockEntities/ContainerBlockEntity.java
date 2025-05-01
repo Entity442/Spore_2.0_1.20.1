1e5968376bea043d1530ed5c96642bec51527dcb
mihai <mihailungoci87@gmail.com>
2025-05-01 06:04:07 -0700

    cabinet and documents

--- src/main/java/com/Harbinger/Spore/SBlockEntities/ContainerBlockEntity.java
+++ src/main/java/com/Harbinger/Spore/SBlockEntities/ContainerBlockEntity.java
@@ -4,7 +4,6 @@ import com.Harbinger.Spore.Core.SblockEntities;
 import com.Harbinger.Spore.Screens.ContainerMenu;
 import io.netty.buffer.Unpooled;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
 import net.minecraft.core.NonNullList;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.Connection;
@@ -12,17 +11,14 @@ import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
 import net.minecraft.world.ContainerHelper;
-import net.minecraft.world.WorldlyContainer;
 import net.minecraft.world.entity.player.Inventory;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.block.entity.BaseContainerBlockEntity;
+import net.minecraft.world.level.block.entity.RandomizableContainerBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.Nullable;
 
-public class ContainerBlockEntity extends BaseContainerBlockEntity implements WorldlyContainer {
-    private static final int[] slotsTop = new int[]{0};
+public class ContainerBlockEntity extends RandomizableContainerBlockEntity {
     private NonNullList<ItemStack> stacks = NonNullList.withSize(32, ItemStack.EMPTY);
     public ContainerBlockEntity(BlockPos pos, BlockState state) {
         super(SblockEntities.CONTAINER.get(),pos , state);
@@ -44,6 +40,13 @@ public class ContainerBlockEntity extends BaseContainerBlockEntity implements Wo
     public AbstractContainerMenu createMenu(int id, Inventory inventory) {
         return new ContainerMenu(id, inventory, new FriendlyByteBuf(Unpooled.buffer()).writeBlockPos(this.worldPosition));
     }
+    @Override
+    public void onDataPacket(Connection net, ClientboundBlockEntityDataPacket packet) {
+        if (packet != null && packet.getTag() != null) {
+            this.stacks = NonNullList.withSize(this.getContainerSize(), ItemStack.EMPTY);
+            ContainerHelper.loadAllItems(packet.getTag(), this.stacks);
+        }
+    }
 
     @Override
     public int getContainerSize() {
@@ -51,93 +54,30 @@ public class ContainerBlockEntity extends BaseContainerBlockEntity implements Wo
     }
 
     @Override
-    public boolean isEmpty() {
-        return false;
-    }
-
-    @Override
-    public ItemStack getItem(int index) {
-        return this.stacks.get(index);
-    }
-
-    @Override
-    public ItemStack removeItem(int index, int count) {
-        if (!this.stacks.get(index).isEmpty()) {
-            ItemStack itemstack;
-
-            if (this.stacks.get(index).getCount() <= count) {
-                itemstack = this.stacks.get(index);
-                this.stacks.set(index, ItemStack.EMPTY);
-            } else {
-                itemstack = this.stacks.get(index).split(count);
-
-                if (this.stacks.get(index).isEmpty()) {
-                    this.stacks.set(index, ItemStack.EMPTY);
-                }
-
-            }
-            return itemstack;
-        } else {
-            return ItemStack.EMPTY;
-        }
-    }
-
-    @Override
-    public ItemStack removeItemNoUpdate(int index) {
-        return null;
-    }
-
-    @Override
-    public void setItem(int index, ItemStack stack) {
-        this.stacks.set(index, stack);
-        if (!stack.isEmpty() && stack.getCount() > this.getMaxStackSize()) {
-            stack.setCount(this.getMaxStackSize());
-        }
-        this.saveAdditional(this.getUpdateTag());
-    }
-
-    @Override
-    public boolean stillValid(Player player) {
-        return true;
-    }
-
     protected void saveAdditional(CompoundTag compound) {
         super.saveAdditional(compound);
-        ContainerHelper.saveAllItems(compound, this.stacks);
+        if (!this.trySaveLootTable(compound)) {
+            ContainerHelper.saveAllItems(compound, this.stacks);
+        }
     }
 
+    @Override
     public void load(CompoundTag compound) {
         super.load(compound);
         this.stacks = NonNullList.withSize(this.getContainerSize(), ItemStack.EMPTY);
-        ContainerHelper.loadAllItems(compound, this.stacks);
-    }
-
-    @Override
-    public int[] getSlotsForFace(Direction p_19238_) {
-        return slotsTop;
-    }
-
-    @Override
-    public boolean canPlaceItemThroughFace(int index, ItemStack stack, @Nullable Direction direction) {
-        return true;
-    }
-
-    @Override
-    public boolean canTakeItemThroughFace(int index, ItemStack stack, Direction direction) {
-        return false;
-    }
-
-    @Override
-    public void clearContent() {
-        this.stacks.clear();
-    }
-
-    @Override
-    public void onDataPacket(Connection net, ClientboundBlockEntityDataPacket packet) {
-        if (packet != null && packet.getTag() != null) {
-            this.stacks = NonNullList.withSize(this.getContainerSize(), ItemStack.EMPTY);
-            ContainerHelper.loadAllItems(packet.getTag(), this.stacks);
+        if (!this.tryLoadLootTable(compound)) {
+            ContainerHelper.loadAllItems(compound, this.stacks);
         }
+    }
+    @Override
+    protected NonNullList<ItemStack> getItems() {
+        this.unpackLootTable(null);
+        return stacks;
+    }
+
+    @Override
+    protected void setItems(NonNullList<ItemStack> nonNullList) {
+        stacks = nonNullList;
     }
 }
 
