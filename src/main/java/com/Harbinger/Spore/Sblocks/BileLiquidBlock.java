bd8594a6a561e078a2fb3a710d12c17f33dba049
mihai <mihailungoci87@gmail.com>
2025-05-01 07:08:36 -0700

    cabinet and documents

--- src/main/java/com/Harbinger/Spore/Sblocks/BileLiquidBlock.java
+++ src/main/java/com/Harbinger/Spore/Sblocks/BileLiquidBlock.java
@@ -1,12 +1,30 @@
 package com.Harbinger.Spore.Sblocks;
 
+import com.Harbinger.Spore.Core.Sblocks;
+import com.Harbinger.Spore.Core.Sfluids;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.block.LiquidBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.FlowingFluid;
-import net.minecraftforge.registries.RegistryObject;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.MapColor;
+import net.minecraft.world.level.material.PushReaction;
 
 public class BileLiquidBlock extends LiquidBlock {
-    public BileLiquidBlock(RegistryObject<FlowingFluid> flowingFluid, BlockBehaviour.Properties properties) {
-        super(flowingFluid, properties);
+    public BileLiquidBlock() {
+        super(Sfluids.Bile_FLUID_SOURCE, BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_ORANGE).noCollission().strength(100.0F).lightLevel(state -> 1).emissiveRendering((state, world, pos) -> false).noLootTable().replaceable().liquid().pushReaction(PushReaction.DESTROY));
+    }
+    @Override
+    public void randomTick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
+        super.randomTick(state, level, pos, random);
+        if (level.getBlockState(pos.above()).isAir() && Math.random() < 0.3f) {
+            level.setBlockAndUpdate(pos, Sblocks.CRUSTED_BILE.get().defaultBlockState());
+        }
+    }
+
+    @Override
+    public boolean isRandomlyTicking(BlockState state) {
+        return true;
     }
 }
