cc8613f506145b91f47f19168fc9a962f0fb57e5
mihai <mihailungoci87@gmail.com>
2025-06-23 06:30:13 -0700

    protector

--- src/main/java/com/Harbinger/Spore/Sitems/PCI.java
+++ src/main/java/com/Harbinger/Spore/Sitems/PCI.java
@@ -7,6 +7,7 @@ import com.Harbinger.Spore.Core.Ssounds;
 import com.Harbinger.Spore.Sentities.BaseEntities.Infected;
 import com.google.common.collect.ImmutableMultimap;
 import com.google.common.collect.Multimap;
+import net.minecraft.ChatFormatting;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
@@ -111,6 +112,7 @@ public class PCI extends BaseItem2 implements CustomModelArmorData,Vanishable {
     @Override
     public void appendHoverText(ItemStack stack, @Nullable Level p_41422_, List<Component> components, TooltipFlag p_41424_) {
         super.appendHoverText(stack, p_41422_, components, p_41424_);
+        components.add(Component.literal("Portable Cryogenic Injector").withStyle(ChatFormatting.BLUE));
         components.add(Component.literal("Charge "+getCharge(stack)+"/"+SConfig.SERVER.pci_max_charge.get()));
     }
 }
