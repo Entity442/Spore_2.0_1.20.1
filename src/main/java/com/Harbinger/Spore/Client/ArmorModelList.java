97bdcc92302f95f2b4d9dc7dabea7e2900496dbc
mihai <mihailungoci87@gmail.com>
2025-07-02 00:01:44 -0700

    some code cleaning

--- src/main/java/com/Harbinger/Spore/Client/ArmorModelList.java
+++ src/main/java/com/Harbinger/Spore/Client/ArmorModelList.java
@@ -92,15 +92,15 @@ public class ArmorModelList {
     }
     public static List<Quad> modelItemMap(HumanoidModel<LivingEntity> humanoidModel){
         List<Quad> map = new ArrayList<>();
-        map.add(new Quad(EquipmentSlot.FEET, Sitems.LIVING_BOOTS.get().asItem(),humanoidModel.leftLeg,boots,boots.leftBoot,-0.15f,-0.8f,0,1.1f));
-        map.add(new Quad(EquipmentSlot.FEET, Sitems.LIVING_BOOTS.get().asItem(),humanoidModel.rightLeg,boots,boots.rightBoot,0.15f,-0.8f,0,1.1f));
-        map.add(new Quad(EquipmentSlot.LEGS, Sitems.LIVING_PANTS.get().asItem(),humanoidModel.leftLeg,boots,boots.leftLeg,-0.15f,-0.8f,0,1.1f));
-        map.add(new Quad(EquipmentSlot.LEGS, Sitems.LIVING_PANTS.get().asItem(),humanoidModel.rightLeg,boots,boots.rightLeg,0.15f,-0.8f,0,1.1f));
-        map.add(new Quad(EquipmentSlot.LEGS, Sitems.LIVING_PANTS.get().asItem(),humanoidModel.body,boots,boots.pelvis,0,0,0,1));
-        map.add(new Quad(EquipmentSlot.CHEST, Sitems.LIVING_CHEST.get().asItem(),humanoidModel.body,boots,boots.body,0,0,0,1.1f));
-        map.add(new Quad(EquipmentSlot.CHEST, Sitems.LIVING_CHEST.get().asItem(),humanoidModel.rightArm,boots,boots.rightArm,0.35f,-0.125f,0,1.1f));
-        map.add(new Quad(EquipmentSlot.CHEST, Sitems.LIVING_CHEST.get().asItem(),humanoidModel.leftArm,boots,boots.leftArm,-0.35f,-0.125f,0,1.1f));
-        map.add(new Quad(EquipmentSlot.HEAD, Sitems.LIVING_HELMET.get().asItem(),humanoidModel.head,boots,boots.headwear,0f,0f,0,1.1f));
+        map.add(new Quad(EquipmentSlot.FEET, Sitems.LIVING_BOOTS.get().asItem(),humanoidModel.leftLeg,boots,boots.leftBoot,-0.15f,-0.8f,0,1.2f));
+        map.add(new Quad(EquipmentSlot.FEET, Sitems.LIVING_BOOTS.get().asItem(),humanoidModel.rightLeg,boots,boots.rightBoot,0.15f,-0.8f,0,1.2f));
+        map.add(new Quad(EquipmentSlot.LEGS, Sitems.LIVING_PANTS.get().asItem(),humanoidModel.leftLeg,boots,boots.leftLeg,-0.15f,-0.8f,0,1.2f));
+        map.add(new Quad(EquipmentSlot.LEGS, Sitems.LIVING_PANTS.get().asItem(),humanoidModel.rightLeg,boots,boots.rightLeg,0.15f,-0.8f,0,1.2f));
+        map.add(new Quad(EquipmentSlot.LEGS, Sitems.LIVING_PANTS.get().asItem(),humanoidModel.body,boots,boots.pelvis,0,0,0,1.2f));
+        map.add(new Quad(EquipmentSlot.CHEST, Sitems.LIVING_CHEST.get().asItem(),humanoidModel.body,boots,boots.body,0,0,0,1.2f));
+        map.add(new Quad(EquipmentSlot.CHEST, Sitems.LIVING_CHEST.get().asItem(),humanoidModel.rightArm,boots,boots.rightArm,0.35f,-0.125f,0,1.2f));
+        map.add(new Quad(EquipmentSlot.CHEST, Sitems.LIVING_CHEST.get().asItem(),humanoidModel.leftArm,boots,boots.leftArm,-0.35f,-0.125f,0,1.2f));
+        map.add(new Quad(EquipmentSlot.HEAD, Sitems.LIVING_HELMET.get().asItem(),humanoidModel.head,boots,boots.headwear,0f,0f,0,1.2f));
         map.add(new Quad(EquipmentSlot.HEAD, Sitems.GAS_MASK.get().asItem(),humanoidModel.head,gasMaskModel,gasMaskModel.GasMask,0f,-0.05f,0,1f));
         map.add(new Quad(EquipmentSlot.CHEST, Sitems.R_ELYTRON.get().asItem(),humanoidModel.body,elytrumModel,elytrumModel.body,0f,0f,-0.1f,1f));
         map.add(new Quad(EquipmentSlot.CHEST, Sitems.INF_UP_CHESTPLATE.get().asItem(),humanoidModel.body,elytrumModel,elytrumModel.body,0f,0f,-0.1f,1f));
