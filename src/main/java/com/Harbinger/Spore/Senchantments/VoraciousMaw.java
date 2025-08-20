c16fe19911962ce57870bfc0bf1916ba670e19ab
mihai <mihailungoci87@gmail.com>
2025-08-20 13:36:17 -0700

    howler variant

--- src/main/java/com/Harbinger/Spore/Senchantments/VoraciousMaw.java
+++ src/main/java/com/Harbinger/Spore/Senchantments/VoraciousMaw.java
@@ -2,6 +2,7 @@ package com.Harbinger.Spore.Senchantments;
 
 import com.Harbinger.Spore.Core.Senchantments;
 import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
@@ -9,6 +10,7 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.player.Player;
 
 public class VoraciousMaw extends BaseSporeEnchantment {
     public VoraciousMaw(EquipmentSlot... slots) {
@@ -19,9 +21,9 @@ public class VoraciousMaw extends BaseSporeEnchantment {
     public void doPostAttack(LivingEntity livingEntity, Entity entity, int value) {
         super.doPostAttack(livingEntity, entity, value);
         if (livingEntity.getItemBySlot(EquipmentSlot.HEAD).getEnchantmentLevel(this) > 0 && entity instanceof Mob){
-            if (Math.random() < 0.3f){
-                livingEntity.playSound(SoundEvents.GENERIC_EAT);
-                livingEntity.addEffect(new MobEffectInstance(MobEffects.SATURATION,40,0));
+            if (Math.random() < 0.1f && livingEntity instanceof Player player){
+                player.playNotifySound(SoundEvents.GENERIC_EAT, SoundSource.AMBIENT,1,1);
+                player.addEffect(new MobEffectInstance(MobEffects.SATURATION,40,0));
             }
         }
     }
