02acc27b874981abd0289c552753766cf1336cd1
mihai <mihailungoci87@gmail.com>
2025-08-19 23:33:07 -0700

    howler variant

--- src/main/java/com/Harbinger/Spore/Sitems/UpgradedInfectedExoskeleton.java
+++ src/main/java/com/Harbinger/Spore/Sitems/UpgradedInfectedExoskeleton.java
@@ -173,6 +173,7 @@ public class UpgradedInfectedExoskeleton extends SporeBaseArmor implements Custo
             if (this.knockback > 0.0F) {
                 builder.put(Attributes.KNOCKBACK_RESISTANCE, new AttributeModifier(uuid, "Armor knockback resistance", (this.knockback + modifyKnockbackResistance(stack,knockback)) * 0.1f, AttributeModifier.Operation.ADDITION));
             }
+            builder.put(ForgeMod.STEP_HEIGHT_ADDITION.get(), new AttributeModifier(uuid, "Armor Step modifier", 0.75, AttributeModifier.Operation.ADDITION));
             if (this.getVariant(stack) == SporeArmorMutations.DROWNED){
                 builder.put(ForgeMod.SWIM_SPEED.get(), new AttributeModifier(uuid, "Armor Speed modifier", 0.25, AttributeModifier.Operation.ADDITION));
             }
@@ -204,7 +205,6 @@ public class UpgradedInfectedExoskeleton extends SporeBaseArmor implements Custo
                 builder.put(Attributes.KNOCKBACK_RESISTANCE, new AttributeModifier(uuid, "Armor knockback resistance", (this.knockback + modifyKnockbackResistance(stack,knockback)) * 0.1f, AttributeModifier.Operation.ADDITION));
             }
             builder.put(ForgeMod.SWIM_SPEED.get(), new AttributeModifier(uuid, "Armor Speed modifier", this.getVariant(stack) == SporeArmorMutations.DROWNED ? 0.75 : 0.5, AttributeModifier.Operation.ADDITION));
-            builder.put(ForgeMod.STEP_HEIGHT_ADDITION.get(), new AttributeModifier(uuid, "Armor Step modifier", 0.75, AttributeModifier.Operation.ADDITION));
             if (this.getVariant(stack) == SporeArmorMutations.REINFORCED || this.getVariant(stack) == SporeArmorMutations.SKELETAL){
                 builder.put(Attributes.MOVEMENT_SPEED, new AttributeModifier(uuid, "Armor Speed modifier", this.getVariant(stack) == SporeArmorMutations.REINFORCED ? -0.01 : 0.01, AttributeModifier.Operation.ADDITION));
             }
