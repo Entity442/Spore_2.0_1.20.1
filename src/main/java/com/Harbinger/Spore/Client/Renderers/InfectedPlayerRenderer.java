5d93ff7a0581051aa9916bd9650bfa85fad9f03c
mihai <mihailungoci87@gmail.com>
2025-09-07 08:35:12 -0700

    infected players reworks

--- src/main/java/com/Harbinger/Spore/Client/Renderers/InfectedPlayerRenderer.java
+++ src/main/java/com/Harbinger/Spore/Client/Renderers/InfectedPlayerRenderer.java
@@ -72,7 +72,7 @@ public class InfectedPlayerRenderer extends BaseInfectedRenderer<InfectedPlayer 
     public static final Map<Component,ResourceLocation> SPECIAL_SKINS =new HashMap<>(){{
         put(Component.literal("Technoblade"),new ResourceLocation(Spore.MODID,
                 "textures/entity/player/techno_skin.png"));
-        put(Component.literal("Coda"),new ResourceLocation(Spore.MODID,
+        put(Component.literal("CODATOWER"),new ResourceLocation(Spore.MODID,
                 "textures/entity/player/inf_coda_skin.png"));
     }};
 
