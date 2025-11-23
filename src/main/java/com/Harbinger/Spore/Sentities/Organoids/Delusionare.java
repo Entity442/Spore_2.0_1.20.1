11888513ae12f867716dc40ab7653cbeca007d92
mihai <mihailungoci87@gmail.com>
2025-11-23 02:41:23 -0800

    usurper

--- src/main/java/com/Harbinger/Spore/Sentities/Organoids/Delusionare.java
+++ src/main/java/com/Harbinger/Spore/Sentities/Organoids/Delusionare.java
@@ -337,7 +337,7 @@ public class Delusionare extends Organoid implements VariantKeeper {
                             }
                         }
                     }else {
-                        living.addEffect(new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE,12000,random.nextInt(3),false,false,false));
+                        living.addEffect(new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE,12000,random.nextInt(2),false,false,false));
                     }
                 }
             }
