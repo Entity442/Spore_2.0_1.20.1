23fc95c854d11c36fcd97c5f61e0282f446517c0
mihai <mihailungoci87@gmail.com>
2025-12-23 01:55:28 -0800

    scent rework

--- src/main/java/com/Harbinger/Spore/Sentities/Utility/ScentEntity.java
+++ src/main/java/com/Harbinger/Spore/Sentities/Utility/ScentEntity.java
@@ -2,10 +2,14 @@ package com.Harbinger.Spore.Sentities.Utility;
 
 import com.Harbinger.Spore.Core.SConfig;
 import com.Harbinger.Spore.Core.Sparticles;
+import com.Harbinger.Spore.ExtremelySusThings.CustomJsonReader.SporeMobConversionData;
 import com.Harbinger.Spore.Sentities.BaseEntities.Infected;
 import com.Harbinger.Spore.Sentities.BaseEntities.UtilityEntity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.nbt.StringTag;
+import net.minecraft.nbt.Tag;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
@@ -25,11 +29,16 @@ import net.minecraft.world.phys.AABB;
 import net.minecraftforge.registries.ForgeRegistries;
 
 import javax.annotation.Nullable;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Random;
 
 public class ScentEntity extends UtilityEntity {
     private static final EntityDataAccessor<Boolean> OVERCHARGED = SynchedEntityData.defineId(ScentEntity.class, EntityDataSerializers.BOOLEAN);
+    private static final EntityDataAccessor<Integer> DISSIPATE = SynchedEntityData.defineId(ScentEntity.class, EntityDataSerializers.INT);
+    private static final EntityDataAccessor<Integer> SUMMON = SynchedEntityData.defineId(ScentEntity.class, EntityDataSerializers.INT);
+    public List<String> idList = new ArrayList<>();
     public ScentEntity(EntityType<? extends PathfinderMob> mob, Level level) {
         super(mob, level);
     }
@@ -42,15 +51,20 @@ public class ScentEntity extends UtilityEntity {
     @Override
     public void tick() {
         if (this.isAlive()){
-            this.getPersistentData().putInt("dissipate", 1 + this.getPersistentData().getInt("dissipate"));
-            if (this.getPersistentData().getInt("dissipate") >= SConfig.SERVER.scent_life.get()) {
+            if (getDissipate() == 1){
+                addToTheList(this);
+            }
+            setDissipate(getDissipate()+1);
+            if (getDissipate() >= SConfig.SERVER.scent_life.get()) {
                 this.discard();
             }
             if (SConfig.SERVER.scent_summon.get()){
-            this.getPersistentData().putInt("summon", 1 + this.getPersistentData().getInt("summon"));
-            if (this.getPersistentData().getInt("summon") >= SConfig.SERVER.scent_summon_cooldown.get()) {
+            setSummon(getSummon()+1);
+            if (getSummon() >= SConfig.SERVER.scent_summon_cooldown.get()) {
                 if (!this.level().isClientSide && (getOvercharged() || checkForNonInfected(this))){
-                this.Summon(this);}
+                this.Summon(this);
+                    setSummon(0);
+                }
             }}
         }
         super.tick();
@@ -68,6 +82,20 @@ public class ScentEntity extends UtilityEntity {
         return false;
     }
 
+    void addToTheList(Entity entity){
+        AABB boundingBox = entity.getBoundingBox().inflate(100);
+        List<Entity> entities = entity.level().getEntities(entity, boundingBox ,EntitySelector.NO_CREATIVE_OR_SPECTATOR);
+        for (Entity en : entities) {
+            for (String entry : SConfig.SERVER.inf_human_conv.get()) {
+                String[] parts = entry.split("\\|");
+                if (parts.length < 2) continue;
+                if (Objects.equals(en.getEncodeId(), parts[0])){
+                    idList.add(parts[1]);
+                }
+            }
+        }
+    }
+
     public boolean getOvercharged(){return entityData.get(OVERCHARGED);}
     public void setOvercharged(boolean b) {
         this.entityData.set(OVERCHARGED,b);
@@ -77,17 +105,38 @@ public class ScentEntity extends UtilityEntity {
     public void addAdditionalSaveData(CompoundTag tag) {
         super.addAdditionalSaveData(tag);
         tag.putBoolean("overcharged",entityData.get(OVERCHARGED));
+        tag.putInt("summon",entityData.get(SUMMON));
+        tag.putInt("dissipate",entityData.get(DISSIPATE));
+        ListTag teamTag = new ListTag();
+        for (String member : idList) {
+            teamTag.add(StringTag.valueOf(member));
+        }
+        tag.put("entities", teamTag);
     }
 
     @Override
     public void readAdditionalSaveData(CompoundTag tag) {
         super.readAdditionalSaveData(tag);
         entityData.set(OVERCHARGED, tag.getBoolean("overcharged"));
+        entityData.set(SUMMON, tag.getInt("summon"));
+        entityData.set(DISSIPATE, tag.getInt("dissipate"));
+        idList.clear();
+        ListTag teamTag = tag.getList("entities", Tag.TAG_STRING);
+        for (int l = 0; l < teamTag.size(); l++) {
+            idList.add(teamTag.getString(l));
+        }
     }
+
+    public void setSummon(int val){entityData.set(SUMMON,val);}
+    public void setDissipate(int val){entityData.set(DISSIPATE,val);}
+    public int getSummon(){return entityData.get(SUMMON);}
+    public int getDissipate(){return entityData.get(DISSIPATE);}
 
     protected void defineSynchedData() {
         super.defineSynchedData();
         this.entityData.define(OVERCHARGED, false);
+        this.entityData.define(SUMMON, 0);
+        this.entityData.define(DISSIPATE, 0);
     }
     @Override
     public void setNoGravity(boolean ignored) {
@@ -138,17 +187,16 @@ public class ScentEntity extends UtilityEntity {
         int r = random.nextInt(-12, 12);
         int c = random.nextInt(-12, 12);
         List<? extends String> ev = SConfig.SERVER.inf_summon.get();
-
+        boolean bigger = idList.size() > ev.size();
         if (world.isEmptyBlock(new BlockPos((int) this.getX() + r, (int) this.getY() + d, (int) this.getZ() + c))){
             for (int i = 0; i < 1; ++i) {
-                int randomIndex = rand.nextInt(ev.size());
-                ResourceLocation randomElement1 = new ResourceLocation(ev.get(randomIndex));
+                int randomIndex = rand.nextInt(bigger ? idList.size() : ev.size());
+                ResourceLocation randomElement1 = new ResourceLocation(bigger ? idList.get(randomIndex) : ev.get(randomIndex));
                 EntityType<?> randomElement = ForgeRegistries.ENTITY_TYPES.getValue(randomElement1);
                 Mob waveentity = (Mob) randomElement.create(level);
                 assert waveentity != null;
                 waveentity.setPos(entity.getX() + r, entity.getY() + 0.5D + d, entity.getZ() + c);
                 waveentity.finalizeSpawn(world, level.getCurrentDifficultyAt(new BlockPos((int) entity.getX(),(int)  entity.getY(),(int)  entity.getZ())), MobSpawnType.NATURAL, null, null);
-                this.getPersistentData().putInt("summon", 0);
                 if (this.getOvercharged()){
                     List<? extends String> buffer = SConfig.SERVER.scent_effects_buff.get();
                     if(waveentity instanceof Infected infected){
