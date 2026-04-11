e7889eeff0adefe3322ea54ce21b1e9be9eb1576
mihai <mihailungoci87@gmail.com>
2026-04-11 02:58:05 -0700

    screwer slasher

--- src/main/java/com/Harbinger/Spore/Sentities/Organoids/HiveTumor.java
+++ src/main/java/com/Harbinger/Spore/Sentities/Organoids/HiveTumor.java
@@ -93,6 +93,7 @@ public class HiveTumor extends Organoid implements FoliageSpread {
                             break;
                         }
                     }
+                }else {
                     SummonProto(serverLevel);
                 }
             }
